82ee0d2df27aa4f740fd5dfc873a346bf772155a
rominalodolo <lodoloromina@gmail.com>
2021-11-25 14:50:08 +0200

    changes

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -11,11 +11,31 @@ class Rating extends Component {
     return (
       <div>
         <h1>Rating: {this.props.rating}</h1>
-        {this.props.rating >= 1 ? <IoIosStar /> : <IoIosStarOutline />}
-        {this.props.rating >= 2 ? <IoIosStar /> : <IoIosStarOutline />}
-        {this.props.rating >= 3 ? <IoIosStar /> : <IoIosStarOutline />}
-        {this.props.rating >= 4 ? <IoIosStar /> : <IoIosStarOutline />}
-        {this.props.rating >= 5 ? <IoIosStar /> : <IoIosStarOutline />}
+        {this.state.rating >= 1 ? (
+          <IoIosStar onClick={this.handleClick.bind(this, 1)} />
+        ) : (
+          <IoIosStarOutline onClick={this.handleClick.bind(this, 1)} />
+        )}
+        {this.state.rating >= 2 ? (
+          <IoIosStar onClick={this.handleClick.bind(this, 2)} />
+        ) : (
+          <IoIosStarOutline onClick={this.handleClick.bind(this, 2)} />
+        )}
+        {this.state.rating >= 3 ? (
+          <IoIosStar onClick={this.handleClick.bind(this, 3)} />
+        ) : (
+          <IoIosStarOutline onClick={this.handleClick.bind(this, 3)} />
+        )}
+        {this.state.rating >= 4 ? (
+          <IoIosStar onClick={this.handleClick.bind(this, 4)} />
+        ) : (
+          <IoIosStarOutline onClick={this.handleClick.bind(this, 4)} />
+        )}
+        {this.state.rating >= 5 ? (
+          <IoIosStar onClick={this.handleClick.bind(this, 5)} />
+        ) : (
+          <IoIosStarOutline onClick={this.handleClick.bind(this, 5)} />
+        )}
       </div>
     );
   }
