6407bc21cb2ddc7b6d528e84df943dd8ff3b17ef
rominalodolo <lodoloromina@gmail.com>
2021-11-25 14:43:28 +0200

    update

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import { IoIosStar, IoIosStarOutline } from "react-icons/io";
 
 class Rating extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { rating: this.props.rating };
+  }
+
   render() {
     return (
       <div>
         <h1>Rating: {this.props.rating}</h1>
+        {this.props.rating >= 1 ? <IoIosStar /> : <IoIosStarOutline />}
+        {this.props.rating >= 2 ? <IoIosStar /> : <IoIosStarOutline />}
+        {this.props.rating >= 3 ? <IoIosStar /> : <IoIosStarOutline />}
+        {this.props.rating >= 4 ? <IoIosStar /> : <IoIosStarOutline />}
+        {this.props.rating >= 5 ? <IoIosStar /> : <IoIosStarOutline />}
       </div>
     );
   }
