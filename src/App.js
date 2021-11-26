86902ed8772c57c985335d1505f88c434fe2516a
rominalodolo <lodoloromina@gmail.com>
2021-11-26 08:29:51 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>My First React App!</h1>
+        <h1>Beginning React Project</h1>
         <Products />
-        <Button>Default</Button>
+        <Button>Click Me!</Button>
       </div>
     );
   }
