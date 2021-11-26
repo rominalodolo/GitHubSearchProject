8e370a0c60952d0c9f421ff1746f8e93315c66e3
rominalodolo <lodoloromina@gmail.com>
2021-11-26 09:48:31 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
 import Products from "./components/Products.js";
-import { Button } from "react-bootstrap";
+import { Media, Form, FormGroup, FormControl, Button } from 'reactbootstrap';
 import Rating from "./components/Rating.js";
 
 class App extends Component {
   render() {
+    const isValid = true;
+
     return (
       <div>
         <h1>Beginning React Project</h1>
         <Products />
-        <Button>Click Me!</Button>
-        <div>
-          <Rating rating="1" />
-          <Rating rating="2" />
-          <Rating rating="3" />
-          <Rating rating="4" />
-          <Rating rating="5" />
-        </div>
+        <Button variant="primary" disabled={!isValid}>Default</Button>
+        <Rating rating="1" />
+        <Rating rating="2" />
+        <Rating rating="3" />
+        <Rating rating="4" />
+        <Rating rating="5" />
       </div>
     );
   }
--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -1,21 +1,54 @@
 import React, { Component } from "react";
-import axios from "axios"; 
-
+import axios from "axios";
+import ReactLoading from "react-loading";
 
 class GitHub extends Component {
   constructor() {
     super();
+    this.state = {
+      data: [],
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
     this.getGitHubData("greg");
   }
 
   getGitHubData(_searchTerm) {
-    axios.get("https://api.github.com/search/users?q=" + _searchTerm)
+    axios
+      .get("https://api.github.com/search/users?q=" + _searchTerm)
       .then((res) => {
+        this.setState({
+          isLoading: false,
+          data: res.data.items,
+        });
         console.log(res.data.items);
       });
   }
   render() {
-    return <div></div>;
+    const listUsers = this.state.data.map((user) => (
+      <Media key={user.id}>
+        <Media.Left>
+          <a href={user.html_url}>
+            <img width={64} height={64} src={user.avatar_url} alt="Image" />
+          </a>
+        </Media.Left>
+        <Media.Body>
+          <Media.Heading>{user.login}</Media.Heading>
+          <p>Score: {user.score}</p>
+        </Media.Body>
+      </Media>
+    ));
+    return (
+      <div>
+        <h3>GitHub Users Results</h3>
+        {this.state.isLoading && 
+          <ReactLoading type="spinningBubbles" color="#444" />
+        }
+        {listUsers}
+      </div>
+    );
   }
 }
 
