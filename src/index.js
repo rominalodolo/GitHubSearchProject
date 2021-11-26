c80fe63398cbe7333f60f65795542f8c5fee8d6b
rominalodolo <lodoloromina@gmail.com>
2021-11-26 13:21:43 +0200

    users

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// import * as firebase from 'firebase';
+//import * as firebase from 'firebase';
 
 // var config = {
 //     apiKey: "AIzaSyBN9WlmRc3SedmC4agM1G-rYqezGR22iZE",
@@ -19,3 +19,26 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
   <App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAH4CjXrXm51t8ZeaKUfAB1XYPpFxKpzOo",
+//   authDomain: "crudbeginningreact.firebaseapp.com",
+//   projectId: "crudbeginningreact",
+//   storageBucket: "crudbeginningreact.appspot.com",
+//   messagingSenderId: "536739765095",
+//   appId: "1:536739765095:web:1f1a2b5128492fe9b8adc2",
+//   measurementId: "G-FCSG8N2B79"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
