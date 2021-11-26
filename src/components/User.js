812636cb82af1a3b591821e83036e6e65fc365a3
rominalodolo <lodoloromina@gmail.com>
2021-11-26 13:28:13 +0200

    code

---
+++ src/components/User.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import * as firebase from "firebase";
+
+class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+  componentDidMount() {
+    firebase
+      .database()
+      .ref("/")
+      .on("value", (snapshot) => {
+        console.log(snapshot.val());
+      });
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default User;
