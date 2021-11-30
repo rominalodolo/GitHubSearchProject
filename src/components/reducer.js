41de11e04a5f81d630c675db5e178f0bb23515ec
rominalodolo <lodoloromina@gmail.com>
2021-11-30 10:21:34 +0200

    chap11

--- src/components/reducer.js
+++ src/components/reducer.js
@@ -2,33 +2,35 @@
 // Reducer
 
 function cartReducer(state, action) {
-    if (state === undefined) {
+  if (state === undefined) {
     return {
-    totalCost: 0,
-    productCart: []
+      totalCost: 0,
+      productCart: [],
     };
-    }
-    switch (action.type) {
+  }
+
+  switch (action.type) {
     case "addProduct":
-    return {
-    ...state,
-    totalCost: state.totalCost+parseInt(action.productData.productPrice),
-    productCart: state.productCart.concat({
-    productName: action.productData.productName,
-    productPrice: action.productData.productPrice
-    })
-    }
+      return {
+        ...state,
+        totalCost: state.totalCost + parseInt(action.productData.productPrice),
+        productCart: state.productCart.concat({
+          productName: action.productData.productName,
+          productPrice: action.productData.productPrice,
+        }),
+      };
     case "deleteProduct":
-    const updatedArray = state.productCart.filter(product =>
-    product.productName !== action.productData.productName);
-    return{
-    ...state,
-    totalCost: state.totalCost-parseInt(action.productData.productPrice),
-    productCart: updatedArray
-    }
+      const updatedArray = state.productCart.filter(
+        (product) => product.productName !== action.productData.productName
+      );
+      return {
+        ...state,
+        totalCost: state.totalCost - parseInt(action.productData.productPrice),
+        productCart: updatedArray,
+      };
     default:
-return state;
-}
+      return state;
+  }
 }
 
 export default cartReducer;
