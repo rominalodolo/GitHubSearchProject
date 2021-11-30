159dc2df9b0cad4c12107caec3215f1f4b5d5214
rominalodolo <lodoloromina@gmail.com>
2021-11-30 10:02:31 +0200

    updates

--- src/components/User.js
+++ src/components/User.js
@@ -11,19 +11,42 @@ class User extends Component {
     super(props);
     this.state = {
       users: [],
+      showDeleteDialog: false,
+      selectedUser: {}
     };
+    this.add = this.add.bind(this); 
+    this.closeDeleteDialog = this.closeDeleteDialog.bind(this);                                       
+    this.delete = this.delete.bind(this);
   }
-  componentDidMount() {
-    firebase
-      .database()
-      .ref("/")
-      .on("value", (snapshot) => {
-        console.log(snapshot.val());
+  // componentDidMount() {
+  //   firebase
+  //     .database()
+  //     .ref("/")
+  //     .on("value", (snapshot) => {
+  //       console.log(snapshot.val());
+  //     });
+  // }
+  // render() {
+  //   return <div></div>;
+  // }
+  componentDidMount(){
+    firebase.database().ref('/')
+      .on('value',snapshot => {
+        let returnArr = [];        
+        snapshot.forEach(data => {
+            var user = data.val();
+            user['key'] = data.key;
+            returnArr.push(user);        
+        });                       
+        this.setState({
+            users: returnArr
+        })                
       });
-  }
-  render() {
-    return <div></div>;
-  }
 }
+
+add(e) {
+  this.props.history.push("/add");  
+}  
+
 
 export default User;
