632af541ddacc30e6a8a54ac0844e88ec17a9a41
rominalodolo <lodoloromina@gmail.com>
2021-12-06 18:10:34 +0200

    update

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
-import Rating from './Rating.js';
-import { Modal } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import React, { Component } from 'react';
+import Rating from './Rating';
+import { Media } from 'react-bootstrap';
 
 class Product extends Component {
 
   constructor(props){
-    super(props);
+    super(props);    
   }
 
-  render() {
+  render() { 
     return (
-      <div>
-        <Modal>
-          <img
-            width={64}
-            height={64}
-            className="mr-3"
-            src={this.props.data.imageUrl}
-            alt="Portrait"
-          />
-          <Modal.Body>
-            <h5>{this.props.data.productName}</h5>
-            {this.props.data.releasedDate}
-            <Rating
-              rating={this.props.data.rating}
-              numOfReviews={this.props.data.numOfReviews}
+      <div>                
+        <Media>
+            <img 
+                width={64} 
+                height={64} 
+                className="mr-3"
+                src={this.props.data.imageUrl} 
+                alt="Portrait"
             />
-            <p>{this.props.data.description}</p>
-          </Modal.Body>
-        </Modal>
+            <media-body>
+                <h5>{this.props.data.productName}</h5>
+                { this.props.data.releasedDate }
+                <Rating rating={this.props.data.rating} numOfReviews={this.props.data.numOfReviews}/>
+                <p>{this.props.data.description}</p>
+            </media-body>
+        </Media>                                                                                                                                   
       </div>
     );
   }
